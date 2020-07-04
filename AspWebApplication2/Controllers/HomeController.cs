2ad133bfc20c25b004e767f39f90a56045ea604f
Nimmala Surya (NHL8NJV) <NHL8NJV@aad.ups.com>
2020-07-04 19:18:26 -0400

    First ASP Web Application in GUIHUB

---
+++ AspWebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspWebApplication2.Models;
+
+namespace AspWebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+		private MoviesDBEntities _db = new MoviesDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_db.Movies.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+			return View();
+        }
+
+		// POST: Home/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "Id")] Movie movieToCreate)
+        {
+            try
+            {
+				if (!ModelState.IsValid)
+					return View();
+
+				_db.Movies.Add(movieToCreate);
+				_db.SaveChanges();
+
+				return RedirectToAction("Index");
+			}
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+			var movieToEdit = (from m in _db.Movies
+							   where m.Id == id
+							   select m).First();
+            return View(movieToEdit);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Movie movieToEdit)
+        {
+			var originalMovie = (from m in _db.Movies
+								 where m.Id == movieToEdit.Id
+								 select m).First();
+
+			if (!ModelState.IsValid)
+				return View(originalMovie);
+
+			_db.Entry(originalMovie).CurrentValues.SetValues(movieToEdit);
+			_db.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
